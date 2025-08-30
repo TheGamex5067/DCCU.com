c16311f12bbf645b94287155fb772b682376cbab
TheGamex5067 <127174759+TheGamex5067@users.noreply.github.com>
2025-08-30 01:35:14 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function checkPassword() {
-    var password = document.getElementById("password").value;
-    if (password === "DCCU5067") {
-        document.getElementById("content").style.display = "block";
-        document.getElementById("passwordForm").style.display = "none";
-    } else {
-        alert("Incorrect password. Please try again.");
-    }
-}
